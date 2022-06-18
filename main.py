4fa90e521aea0cb6ee20b75bab3bb70ea9aa986c
labian <labian911@gmail.com>
2022-06-18 19:03:49 +0200

    ajoute find_angle

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-import entities
+import entities, cmath
+
+def find_angle(coords1, coords2): #compare le 2e set de coordonnées par rapport au premier
+    x = coords2[0] - coords1[0]
+    y = coords2[1] - coords1[1]
+    return cmath.phase(complex(x, y))
