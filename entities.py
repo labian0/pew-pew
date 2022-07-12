b791b70e13e014c3f90d404d5decf9a457418790
labian <labian911@gmail.com>
2022-07-12 20:50:35 +0200

    meilleur Bullet

--- entities.py
+++ entities.py
@@ -1,4 +1,14 @@
 import cmath
+
+def find_angle(coords1, coords2): #compare le 2e set de coordonnées par rapport au premier
+    x = coords2[0] - coords1[0]
+    y = coords2[1] - coords1[1]
+    return cmath.phase(complex(x, y))
+
+def compare(a, b, biggest: bool): #compare deux nombres, si biggest est True, compare() retournera le plus grand nombre (je comptais appeler ce paramètre "croissant" au début)
+    return (a*(a>b) + b*(b>a))*biggest + (a*(a<b) + b*(b<a))*(not biggest)
+
+print(compare(3, 2, biggest=True))
 class Player():
     def __init__(self, x, y):
         self.coords = [x,y]
@@ -9,9 +19,10 @@ class Player():
         
 
 class Bullet():
-    def __init__(self, x, y, angle, range):
-        self.coords = [x,y]
-        self.angle = angle
+    def __init__(self, start: tuple or list, end: tuple or list, range):
+        self.start_coords = start
+        bigger = compare(end[0], end[1], biggest=True)
+        self.end_coords = ((end[0]/bigger)*range, (end[1]/bigger)*range)
 
     def shoot(self, canvas):    
-        canvas.create_line(0,0,self.coords[0],self.coords[1], fill="white", width=3)
+        canvas.create_line(self.start_coords[0], self.start_coords[1], self.end_coords[0],self.end_coords[1], fill="white", width=3)
--- main.py
+++ main.py
@@ -1,18 +1,13 @@
 from entities import Player, Bullet, cmath
 import tkinter
 
-def find_angle(coords1, coords2): #compare le 2e set de coordonnées par rapport au premier
-    x = coords2[0] - coords1[0]
-    y = coords2[1] - coords1[1]
-    return cmath.phase(complex(x, y))
-
 def main():
     f = tkinter.Tk()
     canvas = tkinter.Canvas(f, height=500, width=500, bg="black", bd=0)
-    
+
     def shoot_bullet(event):
         x,y = event.x,event.y
-        proj = Bullet(x, y, 0, 0)
+        proj = Bullet((0,0),(x, y), 400)
         proj.shoot(canvas)
 
     #le programme va ici
